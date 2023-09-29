86173397626e307e27f067bc054dc2a3eea7f0e6
bhi049 <bhi049@myy.haaga-helia.fi>
2023-09-29 11:35:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function siirykoris() {
+    window.location.href = "koripallo.html";
+}
+function siirytakaisin() {
+    window.location.href = "aboutme.html";
+}
+function siirysivuille() {
+    window.location.href = "aboutme.html";
+}
