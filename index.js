8eb5e442aa23f9ffb58fb450a9927fe24ce0bcb9
bhi049 <bhi049@myy.haaga-helia.fi>
2023-10-03 14:36:27 +0300

    java pois

--- index.js
+++
@@ -1,9 +0,0 @@
-function siirykoris() {
-    window.location.href = "koripallo.html";
-}
-function siirytakaisin() {
-    window.location.href = "aboutme.html";
-}
-function siirysivuille() {
-    window.location.href = "aboutme.html";
-}
